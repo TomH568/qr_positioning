f31f075681c5b385d94cef3605ce5de1078d202c
TomH568 <tomi.hiltunen@hotmail.fi>
2026-03-26 14:53:12 +0200

    Turned the app into an inventory system
    Uses information provided by the JSON in the QR codes. 
    Attributes for inventory: 
    inStock: bool
    name: str
    price: int/float

--- 2504455.js
+++ 2504455.js
@@ -5,10 +5,15 @@ function toggleScanner() {
     scannerOn = !scannerOn;
     if (scannerOn) {
         startScanner();
+        if (inventory.children.length > 0) {
+            inventory.replaceChildren(); // resets the inventory listing if something has already been scanned
+        }
+        inventory.style.display = "none";
         mapContainer.style.display = "none";
         btn.innerText = "CANCEL";
     } else {
         stopScanner();
+        inventory.style.display = "grid";
         mapContainer.style.display = "block";
         btn.innerText = "SCAN";
     }
@@ -21,8 +26,33 @@ function startScanner() {
         function (text) {
             const place = JSON.parse(text); // parsing the JSON from the QR code
             showMarkerAt(place.top, place.left);
-            console.log(text);
-            console.log(place.name);
+
+            // adding inventory information from QR code as paragraphs
+            const labelName = document.createElement("p");
+            labelName.innerHTML = "Name:";
+            inventory.appendChild(labelName);
+            const objectName = document.createElement("p");
+            objectName.innerHTML = place.name;
+            inventory.appendChild(objectName);
+            
+            const labelInStock = document.createElement("p");
+            labelInStock.innerHTML = "In stock:";
+            inventory.appendChild(labelInStock);
+            const objectInStock = document.createElement("p");
+            if (place.inStock) {
+                objectInStock.innerHTML = "1 or more";
+            } else {
+                objectInStock.innerHTML = "0 :(";
+            }
+            inventory.appendChild(objectInStock);
+
+            const labelPrice = document.createElement("p");
+            labelPrice.innerHTML = "Price:";
+            inventory.appendChild(labelPrice);
+            const objectPrice = document.createElement("p");
+            objectPrice.innerHTML = place.price + "€";
+            inventory.appendChild(objectPrice);
+            
             toggleScanner();
         }
     ).catch(function (err) {
